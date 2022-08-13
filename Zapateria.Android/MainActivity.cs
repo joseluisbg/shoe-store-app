787a1e63b14f791e8bf6bf991f2180bb9b7ee99c
José Luis Badillo <45501340+joseluisbg@users.noreply.github.com>
2022-08-12 23:09:25 -0500

    Third general folder backup, for android devices.

---
+++ Zapateria.Android/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Zapateria.Droid
+{
+    [Activity(Label = "Zapateria", Icon = "@mipmap/tienda", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+
+            this.LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ Zapateria.Android/SQliteDB.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using SQLite;
+using System.IO;
+using Zapateria.Droid;
+using Zapateria.Datos;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SQLiteDB))]    
+namespace Zapateria.Droid
+{
+    public class SQLiteDB : ISQLiteDB
+    {
+        public SQLiteAsyncConnection GetConnection()
+        {
+            var ruta = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            var path = Path.Combine(ruta, "ZapateriaSQLite.db3");
+            return new SQLiteAsyncConnection(path);
+        }
+    }
+}
